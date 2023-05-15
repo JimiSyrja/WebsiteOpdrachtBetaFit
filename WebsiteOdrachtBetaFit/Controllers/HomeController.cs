92c36701ed741cf4c8b8be34a1ad967310e7145c
Zujan Selvanayagam <111127@st-maartenscollege.nl>
2023-05-15 11:42:57 +0200

    ND; Werkende api, probleem opgelost

--- WebsiteOdrachtBetaFit/Controllers/HomeController.cs
+++ WebsiteOdrachtBetaFit/Controllers/HomeController.cs
@@ -45,7 +45,7 @@ namespace WebsiteOdrachtBetaFit.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new TheBios.Models.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
